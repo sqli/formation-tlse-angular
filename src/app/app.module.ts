a667790048233c79a7be710e3c51d9678153fd19
Mauny Matthieu <mmauny@sqli.com>
2017-08-07 17:19:53 +0200

    initialization practice02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MdListModule} from '@angular/material';
+import {MdToolbarModule} from '@angular/material';
+import {MdCardModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdListModule,
+    MdToolbarModule,
+    MdCardModule, 
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
