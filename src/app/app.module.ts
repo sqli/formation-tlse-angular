3b983b2b6155dfe615d2dce1624062b203c20771
Mauny Matthieu <mmauny@sqli.com>
2017-08-11 14:33:23 +0200

    style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import {MdListModule} from '@angular/material';
-import {MdToolbarModule} from '@angular/material';
-import {MdCardModule} from '@angular/material';
+import { MdListModule } from '@angular/material';
+import { MdToolbarModule } from '@angular/material';
+import { MdCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BookComponent } from './book/book.component';
 
 @NgModule({
@@ -21,7 +21,7 @@ import { BookComponent } from './book/book.component';
     FlexLayoutModule,
     MdListModule,
     MdToolbarModule,
-    MdCardModule, 
+    MdCardModule,
     BrowserAnimationsModule
   ],
   providers: [],
