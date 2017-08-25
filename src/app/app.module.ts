ac52078bffcac43713cdf7e8df39dd10272e2903
Mauny Matthieu <mmauny@sqli.com>
2017-08-25 10:18:00 +0200

    lint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserModule,
     MdListModule,
     MdToolbarModule,
-    MdCardModule, 
+    MdCardModule,
     BrowserAnimationsModule
   ],
   providers: [],
