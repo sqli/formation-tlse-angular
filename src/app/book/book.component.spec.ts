96ed175f1bc404bba4a7137be6c2225d5c091165
Mauny Matthieu <mmauny@sqli.com>
2017-09-04 16:58:13 +0200

    testing

--- src/app/book/book.component.spec.ts
+++ src/app/book/book.component.spec.ts
@@ -2,13 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
 
+import { MdCardModule } from '@angular/material';
+
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+      imports: [MdCardModule]
     })
     .compileComponents();
   }));
@@ -16,6 +19,7 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+    component.book = {id: 1,title:"Germinal", author:"zola"} 
     fixture.detectChanges();
   });
 
