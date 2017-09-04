cc0a9068b04fc40329b132c18269079ed3df9b12
Mauny Matthieu <mmauny@sqli.com>
2017-09-04 17:13:07 +0200

    testing

--- src/app/book/book.component.spec.ts
+++ src/app/book/book.component.spec.ts
@@ -2,13 +2,16 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
 
+import { MdCardModule } from '@angular/material';
+
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+      imports: [MdCardModule]
     })
     .compileComponents();
   }));
--- src/app/shared/uppercase.directive.spec.ts
+++ src/app/shared/uppercase.directive.spec.ts
@@ -1,8 +1,14 @@
 import { UppercaseDirective } from './uppercase.directive';
+import { ElementRef } from '@angular/core';
+
+export class MockElementRef extends ElementRef {
+  constructor() { super({ style: {} }); }
+}
 
 describe('UppercaseDirective', () => {
   it('should create an instance', () => {
-    const directive = new UppercaseDirective();
+    const el = new MockElementRef();
+    const directive = new UppercaseDirective(el);
     expect(directive).toBeTruthy();
   });
 });
