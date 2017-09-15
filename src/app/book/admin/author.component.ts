596d0f0b614ffc2f528b0767660d32b074a12d96
Mauny Matthieu <mmauny@sqli.com>
2017-09-15 18:55:14 +0200

    form validation

--- src/app/book/admin/author.component.ts
+++ src/app/book/admin/author.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Author } from '../Author';
 import { AuthorService } from '../author.service';
 
@@ -10,9 +10,13 @@ import { AuthorService } from '../author.service';
 })
 export class AuthorComponent implements OnInit {
   authorForm = new FormGroup({
-    name: new FormControl()
+    name: new FormControl(
+      '',
+      [
+        Validators.required,
+        Validators.minLength(4)
+      ])
   });
-
 
   constructor(private authorService: AuthorService) { }
 
